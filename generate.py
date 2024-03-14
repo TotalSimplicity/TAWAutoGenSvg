0d8453a5a62b59e905bb0d917e8ff3e074529720
TotalSimplicity <68793354+TotalSimplicity@users.noreply.github.com>
2024-03-14 08:16:33 -0400

    Switch to 1x7

--- generate.py
+++ generate.py
@@ -1,17 +1,14 @@
 import random
 import csvparser
 
-# Read the SVG template from file
 with open("emptytag.txt", "r") as f:
     design = f.read()
 
-# Parse CSV data
 prefixes, names, titles = csvparser.parseCsv()
 
-# Maximum characters allowed per line
 masteri = 0
 
-platesPerSheet = 15
+platesPerSheet = 7
 
 sheetnum = 1
 
@@ -19,26 +16,22 @@ def genSheet():
     global masteri
     global sheetnum
     i = 0
-    # Copy the template for each sheet
     sheet_design = design
     while i <= platesPerSheet:
         name = names[masteri]
         prefix = prefixes[masteri]
         title = titles[masteri]
-        # Replace placeholders with actual data
         sheet_design = sheet_design.replace("NAME", name, 1)
         sheet_design = sheet_design.replace("PREFIX", prefix, 1)
         sheet_design = sheet_design.replace("TITLE", title, 1)
         i += 1
         masteri += 1
-        print(masteri)
+        print(prefix + name)
         if masteri == len(names):
             break
-    # Write the filled sheet to a file
     with open(f"sheets/{sheetnum}filledsheet.svg", "w") as f:
         f.write(sheet_design)
         sheetnum+=1
 
-# Generate sheets until reaching the maximum count
-while masteri < len(names):
+while masteri <= len(names):
     genSheet()
